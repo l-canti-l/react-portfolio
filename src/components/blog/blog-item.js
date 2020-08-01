59e1c7ce85a27c989de77744b45817e0f5ed3034
l-Canti-l <ljdemar91@gmail.com>
2020-07-31 20:45:09 -0500

    added dropzone connectivity

--- src/components/blog/blog-item.js
+++ src/components/blog/blog-item.js
@@ -1,21 +1,34 @@
 import React from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import striptags from "striptags";
+import Truncate from "react-truncate";
 
-const BlogItem = props => {
+const BlogItem = (props) => {
   const {
     id,
     blog_status,
     content,
     title,
-    featured_image_url
+    featured_image_url,
   } = props.blogItem;
 
   return (
     <div>
-    <Link to={`/b/${id}`}>
-      <h1>{title}</h1>
-    </Link>
-      <div>{content}</div>
+      <Link to={`/b/${id}`}>
+        <h1>{title}</h1>
+      </Link>
+      <div>
+        <Truncate
+          lines={5}
+          ellipsis={
+            <span>
+              ...<Link to={`/b/${id}`}><br></br>Read More</Link>
+            </span>
+          }
+        >
+          {striptags(content)}
+        </Truncate>
+      </div>
     </div>
   );
 };
